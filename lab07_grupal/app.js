7c30e4bb5161220cfc92a51f0c68df8d6452ceae
Xd <nose@gmail.com>
2024-04-24 17:32:37 -0500

    conexiona  db

--- lab07_grupal/app.js
+++ lab07_grupal/app.js
@@ -2,11 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import logger from "morgan";
 import cors from "cors";
+import { connectDB } from "./database/mongo.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+connectDB().then(() => {
+  app.listen(port, () => {
+     console.log(`Server is running on port ${port}`);
+  });
+ }).catch(err => {
+  console.error('Error al conectar a la base de datos:', err);
+ });
 
 app.use(cors("*"));
 
@@ -16,8 +25,4 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
-});
-
-app.listen(port, () => {
-  console.log(`Server is runing on port ${port}`);
 });
